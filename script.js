e05e2d1ee781809b608999291ef6495614242a06
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-03-27 21:56:16 -0400

    accediendo a los hijos con .children[]

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-console.log(document.body.innerHTML)
-console.log(document.body.innerText)
+console.log(document.body.children[0].innerText)
+document.body.children[0].innerText = 'Texto Nuevo'
+
+document.body.children[1].children[0].innerText = 'He modificado el copy'
