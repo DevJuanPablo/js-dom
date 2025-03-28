c750d808126118615ee2c0514ce637f9d2b3b63b
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-03-27 22:23:48 -0400

    accediendo a la hoja de estilos

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ const route = document.location.href
 bodyChildren[0].innerText = 'Texto Nuevo'
 bodyChildren[1].children[0].innerText = route
 
-document.body.style.backgroundColor = 'tomato'
+//document.body.style.backgroundColor = 'tomato'
+console.log(document.styleSheets[0])
+document.styleSheets[0].cssRules[0].style.backgroundColor = 'green'
