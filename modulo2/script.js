e87895928918c5602e099691ff5c99aedcb1b08a
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-03-29 23:16:03 -0400

    accedemos al tipo del input y modificamos su tipo

--- modulo2/script.js
+++ modulo2/script.js
@@ -1,6 +1,8 @@
 const myForm = document.getElementById('formulario')
 console.log(myForm)
 
-// accede al valor
-console.log(myForm.querySelector('#username').value)
-console.log(myForm.querySelector('#password').value)
+// accede al tipo de input y cambia el tipo de input
+console.log(myForm.querySelector('#username').type)
+console.log(myForm.querySelector('#password').type)
+myForm.querySelector('#username').type = 'email'
+myForm.querySelector('#password').type = 'text'
