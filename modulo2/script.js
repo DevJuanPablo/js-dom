110fac6cf3dfcf14e2dcdc4f269a18fd25dbd947
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-03-29 23:09:51 -0400

    accediendo al valor de los input del form

---
+++ modulo2/script.js
@@ -0,0 +1,6 @@
+const myForm = document.getElementById('formulario')
+console.log(myForm)
+
+// accede al valor
+console.log(myForm.querySelector('#username').value)
+console.log(myForm.querySelector('#password').value)
