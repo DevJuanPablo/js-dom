3129408dbc410b8e7128d13987be9473c2200f38
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-03-29 23:29:38 -0400

    accedemos a los valores de un checkbox

--- modulo2/script.js
+++ modulo2/script.js
@@ -1,8 +1,6 @@
 const myForm = document.getElementById('formulario')
 console.log(myForm)
 
-// verificamos si el campo es requerido
-console.log(myForm.querySelector('#password').required)
-
-// modificamos el campo para que sea requerido
-myForm.querySelector('#password').required = true
+// accedemos a los valores de un checkbox
+const ac = myForm.querySelector('#aceptar')
+console.log(ac.checked)
