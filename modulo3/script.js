55b2448b7ed514edd5cd18c73a873be5dc5a3780
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-03-31 21:20:36 -0400

    agregar eventos directamente (forma 1) no recomendada

--- modulo3/script.js
+++ modulo3/script.js
@@ -1,7 +1,19 @@
-function saludar() {
+const inputUsuario = document.getElementById('username')
+inputUsuario.onclick = clickMouse
+inputUsuario.onmouseover = overMouse
+
+function clickMouse() {
   const arts = document.getElementById('formulario')
   const p = document.createElement('p')
   p.innerHTML = "Esto fue agregado al hacer click"
+
+  arts.appendChild(p)
+}
+
+function overMouse() {
+  const arts = document.getElementById('formulario')
+  const p = document.createElement('p')
+  p.innerHTML = "El mouse ha pasado por encima"
 
   arts.appendChild(p)
 }
