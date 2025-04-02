f8aecc836c8ee76750eaa5002b8c01fcc8fbe36a
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-04-01 21:28:51 -0400

    creacion de la practica, menu que se oculta

---
+++ modulo3/practico/script.js
@@ -0,0 +1,13 @@
+const poster = document.getElementById('poster')
+const file = poster.src
+
+const divBlur = document.querySelector('#blur')
+divBlur.style.backgroundImage = `url(${file})`
+
+/* visualizar y ocultar el menu lateral */
+const menu = document.getElementById('main-menu')
+const flecha = document.getElementById('flecha')
+
+flecha.addEventListener('click', () => {
+  menu.classList.add('hide')
+})
